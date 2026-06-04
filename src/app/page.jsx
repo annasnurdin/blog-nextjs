6c92bb970eaa86d6581d16c0d7c9561ac626ae55
annasnurdin <anasnurdin02@gmail.com>
2026-06-04 15:10:37 +0700

    Navbar: start

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,19 @@
 import Link from "next/link"
 import styles from "./homepage.module.css"
+import Featured from "@/components/featured/Featured"
+import CategoryList from "@/components/categoryList/CategoryList"
+import CardList from "@/components/cardList/CardList"
+import Menu from "@/components/menu/Menu"
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur totam
-        velit dolores iste numquam amet dicta laboriosam possimus unde, eaque
-        rerum fuga asperiores sit perspiciatis consequuntur neque. Id, omnis
-        magni.
-      </h1>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea omnis tenetur
-      reprehenderit laboriosam culpa tempore, ad, quidem quos illum quisquam
-      earum molestiae? Deserunt minima maxime rerum ipsum labore odit quaerat?
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.container}>
+        <CardList />
+        <Menu />
+      </div>
     </div>
   )
 }
