549bcdf8fc7aa98a6688c5ae75ab75e9049291ac
annasnurdin <anasnurdin02@gmail.com>
2026-06-04 13:01:21 +0700

    Layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,18 @@
-import styles from "./homepage.module.css";
+import Link from "next/link"
+import styles from "./homepage.module.css"
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur totam
+        velit dolores iste numquam amet dicta laboriosam possimus unde, eaque
+        rerum fuga asperiores sit perspiciatis consequuntur neque. Id, omnis
+        magni.
+      </h1>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea omnis tenetur
+      reprehenderit laboriosam culpa tempore, ad, quidem quos illum quisquam
+      earum molestiae? Deserunt minima maxime rerum ipsum labore odit quaerat?
+    </div>
+  )
 }
