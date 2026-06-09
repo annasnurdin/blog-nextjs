65b162ec6e35ce4d8c32e093c51020769711a7d7
annasnurdin <anasnurdin02@gmail.com>
2026-06-09 13:09:05 +0700

    Footer: setup

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react"
+import styles from "./footer.module.css"
+import Image from "next/image"
+import Link from "next/link"
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="annas blog logo" width={50} height={50} />
+          <h1 className={styles.logoText}>AnnasSaurus</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus facere
+          nemo perspiciatis, modi distinctio alias commodi obcaecati, itaque,
+          deleniti harum numquam inventore sequi laborum est! Quas eaque
+          voluptates molestias fugit.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="" width={18} height={18} />
+          <Image src="/instagram.png" alt="" width={18} height={18} />
+          <Image src="/youtube.png" alt="" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
